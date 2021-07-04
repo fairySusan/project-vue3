b0634209cf8677750b29823d9ee94c0cd0e6c2d7
fairysusan <792209917@qq.com>
2021-07-04 22:17:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
+<script lang="ts">
+import { defineComponent  } from 'vue'
+import HelloWorld from './components/HelloWorld.vue';
+export default defineComponent({
   components: {
     HelloWorld
   }
-}
+})
 </script>
 
 <style>
